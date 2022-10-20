db9ccca3815ae9bd1739abe7d55e372476acf07a
cromueloliver02 <cromuelbarut@gmail.com>
2022-10-20 19:01:24 +0800

    created custom text field

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/atf_login_avatar.dart';
+import '../widgets/atf_text_field.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -8,14 +9,33 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
 
-    return Scaffold(
-      body: SizedBox(
-        width: screenSize.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            ATFLoginAvatar(),
-          ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: SizedBox(
+          width: screenSize.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const ATFLoginAvatar(),
+              const SizedBox(height: 70),
+              SizedBox(
+                width: screenSize.width * 0.75,
+                child: const ATFTextField(
+                  hintText: 'Email',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+              ),
+              const SizedBox(height: 15),
+              SizedBox(
+                width: screenSize.width * 0.75,
+                child: const ATFTextField(
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/atf_text_field.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ATFTextField extends StatelessWidget {
+  final String hintText;
+  final bool obscureText;
+  final TextInputType keyboardType;
+
+  const ATFTextField({
+    super.key,
+    required this.hintText,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+      ),
+      decoration: InputDecoration(
+        hintText: hintText,
+        contentPadding: const EdgeInsets.symmetric(vertical: 18),
+        hintStyle: const TextStyle(
+          color: Color.fromARGB(255, 230, 230, 230),
+        ),
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 2,
+          ),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 2,
+          ),
+        ),
+      ),
+    );
+  }
+}
